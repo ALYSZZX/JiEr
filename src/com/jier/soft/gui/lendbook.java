2f0674a3c12cd72dc8c6b775e8530c766cf3d72e
忏悔的拾荒者 <@muyu-pc>
2016-11-16 10:28:57 +0800

    借书界面 Change-Id: I4b39f2c13c3b73a9a3b2f4ca6ad7c028d849f564 Signed-off-by: 忏悔的拾荒者 <忏悔的拾荒者@muyu-pc>

---
+++ src/com/jier/soft/gui/lendbook.java
@@ -0,0 +1,89 @@
+package com.jier.soft.gui;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import java.awt.Color;
+import javax.swing.JScrollPane;
+import javax.swing.JButton;
+
+public class lendbook extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField textField;
+	private JTextField textField_1;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					lendbook frame = new lendbook();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public lendbook() {
+		setBackground(Color.DARK_GRAY);
+		setResizable(false);
+		setTitle("\u501F\u4E66");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 737, 490);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel label = new JLabel("\u8BFB\u8005\u7F16\u53F7\uFF1A");
+		label.setBounds(10, 10, 60, 15);
+		contentPane.add(label);
+		
+		JLabel label_1 = new JLabel("\u4E66\u520A\u7F16\u53F7\uFF1A");
+		label_1.setBounds(10, 38, 60, 15);
+		contentPane.add(label_1);
+		
+		textField = new JTextField();
+		textField.setBounds(80, 7, 135, 21);
+		contentPane.add(textField);
+		textField.setColumns(10);
+		
+		textField_1 = new JTextField();
+		textField_1.setColumns(10);
+		textField_1.setBounds(80, 35, 135, 21);
+		contentPane.add(textField_1);
+		
+		JLabel label_2 = new JLabel("\u6240\u6709\u56FE\u4E66\uFF1A");
+		label_2.setBounds(10, 63, 72, 15);
+		contentPane.add(label_2);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(10, 212, 688, -129);
+		contentPane.add(scrollPane);
+		
+		JLabel label_3 = new JLabel("\u672C\u6B21\u501F\u9605\u56FE\u4E66\uFF1A");
+		label_3.setBounds(10, 242, 96, 15);
+		contentPane.add(label_3);
+		
+		JScrollPane scrollPane_1 = new JScrollPane();
+		scrollPane_1.setBounds(-136, 386, 834, -129);
+		contentPane.add(scrollPane_1);
+		
+		JButton button = new JButton("\u501F\u4E66\u786E\u5B9A");
+		button.setBounds(605, 428, 93, 23);
+		contentPane.add(button);
+	}
+}
