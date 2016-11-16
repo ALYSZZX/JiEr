7401a10225412a5887af87deddbc05189cb7f982
ALYSZZX <1606012355@qq.com>
2016-11-16 16:43:08 +0800

    登录
    Change-Id: I502db7bb11345e868d8efe59b9ba4eecf3174a5d
    Signed-off-by: ALYSZZX <1606012355@qq.com>

--- src/com/jier/soft/gui/LoginFrame.java
+++ src/com/jier/soft/gui/LoginFrame.java
@@ -39,38 +39,38 @@ public class LoginFrame extends JFrame implements ActionListener{
 		lblNewLabel.setFont(new Font("閸楀孩鏋冮梾鏈靛姛", Font.PLAIN, 60));
 		lblNewLabel.setBounds(207, 52, 506, 91);
 		contentPane.add(lblNewLabel);
-		
+
 		JLabel lblNewLabel_1 = new JLabel("\u7528\u6237\u540D :");
 		lblNewLabel_1.setFont(new Font("瀵邦喛钂嬮梿鍛寸拨", Font.BOLD, 24));
 		lblNewLabel_1.setBounds(281, 171, 95, 53);
 		contentPane.add(lblNewLabel_1);
-		
-		JLabel label = new JLabel("瀵� 鐮侊細");
+
+		JLabel label = new JLabel("密  码 :");
 		label.setFont(new Font("瀵邦喛钂嬮梿鍛寸拨", Font.BOLD, 24));
 		label.setBounds(281, 264, 95, 53);
 		contentPane.add(label);
-		
+
 		userField = new JTextField();
 		userField.setText("admin");
 		userField.setFont(new Font("瀵邦喛钂嬮梿鍛寸拨", Font.BOLD, 20));
 		userField.setBounds(390, 181, 172, 43);
 		contentPane.add(userField);
 		userField.setColumns(10);
-		
+
 		passwordField = new JPasswordField();
 		passwordField.setFont(new Font("瀵邦喛钂嬮梿鍛寸拨", Font.BOLD, 20));
 		passwordField.setBounds(390, 274, 172, 43);
 		contentPane.add(passwordField);
-		
-		JButton btnNewButton = new JButton("鐧诲綍");
+
+		JButton btnNewButton = new JButton("登录");
 		btnNewButton.setBackground(new Color(245, 222, 179));
 		btnNewButton.setFont(new Font("瀵邦喛钂嬮梿鍛寸拨", Font.BOLD, 20));
 		btnNewButton.setBounds(249, 370, 146, 53);
 		contentPane.add(btnNewButton);
 		btnNewButton.addActionListener(this);
 		btnNewButton.setActionCommand("Login");
-		
-		JButton button = new JButton("閫�嚭");
+
+		JButton button = new JButton("退出");
 		button.setBackground(new Color(245, 222, 179));
 		button.setFont(new Font("瀵邦喛钂嬮梿鍛寸拨", Font.BOLD, 20));
 		button.setBounds(439, 370, 146, 53);
@@ -97,7 +97,7 @@ public class LoginFrame extends JFrame implements ActionListener{
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-	
+
 		if(e.getActionCommand().equals("Exit")){
 			System.exit(0);
 		}
@@ -106,31 +106,32 @@ public class LoginFrame extends JFrame implements ActionListener{
 			char[] pwd = this.passwordField.getPassword();
 			String password = new String(pwd,0,pwd.length);
 			if(username.isEmpty()){
-				JOptionPane.showMessageDialog(this,"鐢ㄦ埛鍚嶄笉鑳戒负绌�","鎻愮ず妗�, JOptionPane.INFORMATION_MESSAGE);
+				JOptionPane.showMessageDialog(this,"用户名不能为空!","提示框", JOptionPane.INFORMATION_MESSAGE);
 			}else if(password.isEmpty()){
-				JOptionPane.showMessageDialog(this,"瀵嗙爜涓嶈兘涓虹┖!","鎻愮ず妗�, JOptionPane.INFORMATION_MESSAGE);
-			}
-			
-			LoginAction action = new LoginAction();
-			boolean flag =action.login(username, password);
-			if(flag){
-				MainPage mf = new MainPage();
-				this.dispose();
-				mf.setVisible(true);
+				JOptionPane.showMessageDialog(this,"密码不能为空!","提示框", JOptionPane.INFORMATION_MESSAGE);
 			}else{
-				JOptionPane.showMessageDialog(this,"鐢ㄦ埛鍚嶆垨瀵嗙爜閿欒!","鎻愮ず妗�, JOptionPane.INFORMATION_MESSAGE);
+
+				LoginAction action = new LoginAction();
+				boolean flag =action.login(username, password);
+				if(flag){
+					MainPage mf = new MainPage();
+					this.dispose();
+					mf.setVisible(true);
+				}else{
+					JOptionPane.showMessageDialog(this,"用户名或密码错误!","提示框", JOptionPane.INFORMATION_MESSAGE);
+				}
 			}
 		}
 	}
 }
 class NewPanel extends JPanel {
-	  public NewPanel() {
+	public NewPanel() {
 
-	  }
-	  public void paintComponent(Graphics g) {
-	   int x = 0, y = 0;
-	   ImageIcon icon = new ImageIcon("image/backgroud.png");// 003.jpg閺勵垱绁寸拠鏇炴禈閻楀洤婀い鍦窗閻ㄥ嫭鐗撮惄顔肩秿娑擄拷
-	   g.drawImage(icon.getImage(), x, y, getSize().width,
-	     getSize().height, this);// 閸ュ墽澧栨导姘冲殰閸斻劎缂夐弨锟�//	    g.drawImage(icon.getImage(), x, y,this);//閸ュ墽澧栨稉宥勭窗閼奉亜濮╃紓鈺傛杹
-	  }
-	 }
+	}
+	public void paintComponent(Graphics g) {
+		int x = 0, y = 0;
+		ImageIcon icon = new ImageIcon("image/backgroud.png");// 003.jpg閺勵垱绁寸拠鏇炴禈閻楀洤婀い鍦窗閻ㄥ嫭鐗撮惄顔肩秿娑擄拷
+		g.drawImage(icon.getImage(), x, y, getSize().width,
+				getSize().height, this);// 閸ュ墽澧栨导姘冲殰閸斻劎缂夐弨锟�//	    g.drawImage(icon.getImage(), x, y,this);//閸ュ墽澧栨稉宥勭窗閼奉亜濮╃紓鈺傛杹
+	}
+}
