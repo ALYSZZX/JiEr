373df11ab7918f4f0f6168af4aee4ff0334ca16b
ALYSZZX <1606012355@qq.com>
2016-11-16 16:50:49 +0800

    居中

--- src/com/jier/soft/Util/Centre.java
+++ src/com/jier/soft/Util/Centre.java
@@ -9,6 +9,7 @@ import javax.swing.JFrame;
 public class Centre {
 	//x，y为窗口大小
 	public static void Centre(JFrame j,int x,int y) {
+		j.setSize(x, y);
 		// 获取工具对象
 		Toolkit tool = Toolkit.getDefaultToolkit();
 		// 获取屏幕尺寸
@@ -19,5 +20,6 @@ public class Centre {
 		y = (d.height-screenInsets.bottom - y) / 2;
 		//设置屏幕居中
 		j.setLocation(x, y);
+		
 	}
 }
