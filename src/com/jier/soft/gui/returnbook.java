b1f1c2a7ac48ec29df0d44a8cd6cf83d8b473761
忏悔的拾荒者 <@muyu-pc>
2016-11-17 11:20:11 +0800

    js
    Change-Id: I33a9f8c3fac7f5848c02b51a65b970cd3b211b9a
    Signed-off-by: 忏悔的拾荒者 <忏悔的拾荒者@muyu-pc>

--- src/com/jier/soft/gui/returnbook.java
+++ src/com/jier/soft/gui/returnbook.java
@@ -5,10 +5,14 @@ import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JTable;
 import javax.swing.border.EmptyBorder;
+import javax.swing.table.DefaultTableModel;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
+
 import java.awt.Color;
+
 import javax.swing.JScrollPane;
 import javax.swing.JButton;
 
@@ -17,6 +21,7 @@ public class returnbook extends JFrame {
 	private JPanel contentPane;
 	private JTextField textField;
 	private JTextField textField_1;
+	private JTable table;
 
 	/**
 	 * Launch the application.
@@ -40,7 +45,7 @@ public class returnbook extends JFrame {
 	public returnbook() {
 		setBackground(Color.DARK_GRAY);
 		setResizable(false);
-		setTitle("\u501F\u4E66");
+		setTitle("\u5F52\u8FD8");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 737, 490);
 		contentPane = new JPanel();
@@ -66,23 +71,39 @@ public class returnbook extends JFrame {
 		textField_1.setBounds(92, 35, 135, 21);
 		contentPane.add(textField_1);
 		
-		JLabel label_2 = new JLabel("\u6240\u6709\u56FE\u4E66\uFF1A");
-		label_2.setBounds(10, 63, 72, 15);
+		JLabel label_2 = new JLabel("\u8BE5\u8BFB\u8005\u6240\u501F\u56FE\u4E66\uFF1A");
+		label_2.setBounds(10, 63, 114, 15);
 		contentPane.add(label_2);
+		
+		String[] title = {"图书名称","出版社","读者编号","读者姓名","借出日期","借用时间","应还日期","续借次数","借出操作员","图书编号"};
+		String[][] content = {}; 
+		
+		DefaultTableModel dt = new DefaultTableModel(content,title);
+		table = new JTable();
 		
 		JScrollPane scrollPane = new JScrollPane();
 		scrollPane.setBounds(10, 212, 688, -129);
+		scrollPane.add(table);
 		contentPane.add(scrollPane);
+		scrollPane.setColumnHeaderView(table);
 		
-		JLabel label_3 = new JLabel("\u672C\u6B21\u501F\u9605\u56FE\u4E66\uFF1A");
+		JLabel label_3 = new JLabel("\u672C\u6B21\u5F52\u8FD8\u56FE\u4E66\uFF1A");
 		label_3.setBounds(10, 242, 96, 15);
 		contentPane.add(label_3);
 		
+		String[] title1 = {"图书编号","图书名称","出版社","借出日期","操作员"};
+		String[][] content1 = {}; 
+		
+		DefaultTableModel dt1 = new DefaultTableModel(content,title);
+		table = new JTable();
+		
 		JScrollPane scrollPane_1 = new JScrollPane();
 		scrollPane_1.setBounds(-136, 386, 834, -129);
+		scrollPane_1.add(table);
 		contentPane.add(scrollPane_1);
+		scrollPane_1.setColumnHeaderView(table);
 		
-		JButton button = new JButton("\u501F\u4E66\u786E\u5B9A");
+		JButton button = new JButton("\u8FD8\u4E66\u786E\u5B9A");
 		button.setBounds(605, 428, 93, 23);
 		contentPane.add(button);
 	}
